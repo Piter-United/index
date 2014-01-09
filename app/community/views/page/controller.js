f6940d5abf41346d134ba382d27c67898f350dde
tamtakoe <tamtakoe@gmail.com>
2014-01-10 01:14:09 +0300

    return lost templates

---
+++ app/community/views/page/controller.js
@@ -0,0 +1,83 @@
+app.controller('CommunityPageCtrl', function($scope, $routeParams, $location, $window, Community, Auth) {
+
+    $window.scrollTo(0,0);
+
+    var creating = $routeParams.communityId ? false : true,
+        emptyCommunity = {
+            logo: "app/community/img/no_logo.jpg"
+        },
+        community = creating ? angular.extend({}, emptyCommunity) : Community.get({id: $routeParams.communityId}),
+        communityUpd = angular.copy(community),
+        newId,
+        editor = {
+            info: creating,
+            note: false
+        },
+        tabs = {
+            events: false,
+            note: true,
+            members: false
+        }
+
+    //Переходим на таб соответствующий url
+    if ($routeParams.tabId) {
+        for (var k in tabs) {
+            tabs[k] = false;
+            if (k == $routeParams.tabId) {
+                tabs[k] = true;
+            }
+        }
+    }
+    $scope.creating = creating;
+    $scope.community = community;
+    $scope.communityUpd = communityUpd;
+    $scope.editor = editor;
+    $scope.tab = tabs;
+
+    $scope.edit = function(key) {
+        angular.extend(communityUpd, community);
+        $scope.editor[key] = true;
+    };
+    $scope.cancel = function(key) {
+        $scope.editor[key] = false;
+    };
+    $scope.create = function(){
+        var date = new Date();
+        communityUpd.date = date.getTime();
+        communityUpd.owner = Auth.$current.user.id;
+        newId = Community.addWithId(communityUpd);
+        $location.path("/community/" + newId);
+    }
+    $scope.save = function(){
+        angular.extend(community, communityUpd);
+        community.$save();
+        editor.info = false;
+        communityUpd = {};
+    }
+    $scope.saveNote = function(){
+        community.note = communityUpd.note;
+        community.$save('note');
+        editor.note = false;
+        communityUpd = {};
+    }
+    $scope.remove = function (){
+        community.$set(null);
+        $location.path("/communities");
+    }
+
+//  TODO: Change url when current tab changed
+//    $scope.$watch('tab', function(newVal, oldVal) {
+//        if (newVal !== oldVal) {
+//            var activeTab
+//            for (var k in newVal) {
+//                if (newVal[k]) {
+//                    if (activeTab) {
+//                        return;
+//                    }
+//                    activeTab = k;
+//                }
+//            }
+//            $location.path("/community/" + $routeParams.communityId + "/" + activeTab);
+//        }
+//    }, true);
+});
