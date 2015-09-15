a4d49e57fc4f21b6de1d9fc821f433de860f473a
nefimov <efimov.nikita@gmail.com>
2015-09-16 00:13:40 +0300

    Basic structure for ITGM#6 landing

---
+++ itgm6/assets/js/site.js
@@ -0,0 +1,65 @@
+(function(){	
+	// ****** GOOGLE MAP *******
+	var map;
+	var brooklyn = new google.maps.LatLng(59.9630562, 30.3163493);
+				
+	var MY_MAPTYPE_ID = 'custom_style';
+				
+	function initialize() {
+				
+		var featureOpts = [
+			{
+				stylers: [
+					{ saturation: -20 },
+					{ lightness: 40 },
+					{ visibility: 'simplified' },
+					{ gamma: 0.8 },
+					{ weight: 0.4 }
+				]
+			},
+			{
+				elementType: 'labels',
+				stylers: [
+					{ visibility: 'on' }
+				]
+			},
+			{
+				featureType: 'water',
+				stylers: [
+					{ color: '#dee8ff' }
+				]
+			}
+		];
+				
+		var mapOptions = {
+			zoom: 17,
+			scrollwheel: false,
+			panControl: false,
+			mapTypeControl: false,
+  			streetViewControl: false,
+			center: new google.maps.LatLng(59.934577, 30.433518),
+			mapTypeControlOptions: {
+				mapTypeIds: [google.maps.MapTypeId.ROADMAP, MY_MAPTYPE_ID]
+			},
+			mapTypeId: MY_MAPTYPE_ID
+		};
+				
+		map = new google.maps.Map(document.getElementById('canvas-map'),mapOptions);
+		var image = 'assets/images/pmarker.png';
+		var myLatLng = new google.maps.LatLng(59.935122, 30.435506);
+		var beachMarker = new google.maps.Marker({
+			position: myLatLng,
+			map: map,
+			icon: image
+		});
+		var styledMapOptions = {
+			name: 'Custom Style'
+		};
+				
+		var customMapType = new google.maps.StyledMapType(featureOpts, styledMapOptions);
+				
+		map.mapTypes.set(MY_MAPTYPE_ID, customMapType);
+	}
+				
+	google.maps.event.addDomListener(window, 'load', initialize); 
+})();
