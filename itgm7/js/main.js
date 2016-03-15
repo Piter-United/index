20461ecb55780634b3766b5210fe5cd138666e6c
Artem <horechek@gmail.com>
2016-03-15 17:49:42 +0300

    Update main.js

--- itgm7/js/main.js
+++ itgm7/js/main.js
@@ -101,7 +101,7 @@ jQuery(document).ready(function($) {
 	//COUNTDOWN TIMER
 	var newYear = new Date(); 
     newYear = new Date(newYear.getFullYear() + 1, 1 - 1, 1); 
-    $('#countdown').countdown({until: new Date(2016, 3-1, 26)}); // enter event day 
+    $('#countdown').countdown({until: new Date(2016, 4-1, 2)}); // enter event day 
     
     $('#removeCountdown').toggle(
         function() {
